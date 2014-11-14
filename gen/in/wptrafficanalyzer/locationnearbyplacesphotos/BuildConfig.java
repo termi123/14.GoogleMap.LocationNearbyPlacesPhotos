f900c621a63c9d304334869d965d7d654d5deb12
termi123 <dung.phantu@gmail.com>
2014-11-14 15:16:42 +0700

    create

---
+++ gen/in/wptrafficanalyzer/locationnearbyplacesphotos/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package in.wptrafficanalyzer.locationnearbyplacesphotos;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
